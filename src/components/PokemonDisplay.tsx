3dbcd96cc282c05bf673e608558d9e43f49e68ea
Victor Nascimento <victornascimento@Victors-MacBook-Pro-2.local>
2022-04-30 23:07:09 -0300

    fix: api only accepts lower case

--- src/components/PokemonDisplay.tsx
+++ src/components/PokemonDisplay.tsx
@@ -18,7 +18,7 @@ export const PokemonDisplay: React.FC<PokemonDisplayProps> = ({ pokemon }) => {
             <div className='bg-red-400 rounded-2xl w-full h-full flex flex-col gap-3 p-4'>
                 <section className=' h-40 w-auto rounded-full bg-stone-50 flex flex-col sm:flex-row justify-center items-center py-2 px-6'>
                     <h1 className='text-stone-700 text-2xl sm:text-7xl xsm:hidden'>
-                        {isDummyDataAngle ? capitalize(dummyPokemon.name) : pokemon.name}
+                        {isDummyDataAngle ? capitalize(dummyPokemon.name) : capitalize(pokemon.name)}
                         </h1>
                     <img
                     className='h-full sm:ml-auto'
--- src/repository/getShakemonRepository.ts
+++ src/repository/getShakemonRepository.ts
@@ -3,7 +3,7 @@ import { Pokemon } from "../models/Pokemon";
 import { api } from "./api";
 
 export const getPokemon = async (name: string) => {
-    return axios.get(`${api.url}/pokemon/${name}`)
+    return axios.get(`${api.url}/pokemon/${name.toLowerCase()}`)
         .then(async (res: AxiosResponse<Pokemon, AxiosError<AxiosResponse<{message: string}>>>) => {
             return res
         })
